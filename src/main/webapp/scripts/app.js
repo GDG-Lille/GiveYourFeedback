a2cc3e88386bbc7fbcd3be293a2818c4047a54f1
Ludovic Borie <l.borie@gmail.com>
2014-04-21 20:11:10 +0200

    Grunt Configuration

---
+++ src/main/webapp/scripts/app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var feedbackApp = angular.module('feedbackApp', ['ngResource', 'ngRoute']);
+var ROOT_API = '/_ah/api/giveyourfeedback/v2';
+var API_VERSION = 'v1beta2';
+var BUCKET = 'conf-picture';
+
+/**
+ * Router
+ */
+feedbackApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller : 'homeController'
+        })
+        .when('/conference',{
+            templateUrl: 'views/conferenceList.html',
+            controller: 'conferenceController'
+        })
+        .when('/conference/:idConference',{
+            templateUrl: 'views/conference.html',
+            controller: 'conferenceController'
+        })
+        .when('/conference/:idConference/:idSession',{
+            templateUrl: 'views/session.html',
+            controller: 'sessionController'
+        })
+        .when('/import', {
+            templateUrl: 'views/import.html',
+            controller : 'importController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
---
+++ src/test/javascript/spec/controllersSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Controllers Tests ', function () {
+
+    beforeEach(module('feedbackApp'));
+
+    describe('sessionController', function () {
+
+        it('should set remember Me', function () {
+            expect(true).toBeTruthy();
+        });
+    });
+
+});
